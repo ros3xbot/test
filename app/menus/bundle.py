3c39426ca842d15db1f5c00e9ba9ec010ebaa370
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-10-11 18:10:10 +0700

    Update bundle.py

--- app/menus/bundle.py
+++ app/menus/bundle.py
@@ -1,6 +1,5 @@
 from app.service.auth import AuthInstance
 from app.service.bookmark import BookmarkInstance
-#from app.service.family_bookmark import FamilyBookmarkInstance
 from app.client.engsel import get_family, get_package, get_package_details
 from app.client.balance import settlement_balance
 from app.client.qris import show_qris_payment
