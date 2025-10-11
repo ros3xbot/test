e4fbea79ac808459fa7e710c89c53ed8d2aee329
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-10-11 07:46:05 +0700

    Update purchase.py

--- app/client/purchase.py
+++ app/client/purchase.py
@@ -1,10 +1,9 @@
-import os
 import json
 import uuid
 import requests
 
 from datetime import datetime, timezone
-
+from app.config.anu_config import BASE_API_URL, AX_DEVICE_ID, AX_FP_KEY, UA
 from app.client.engsel import send_api_request, BASE_API_URL, UA
 from app.client.encrypt import (
     API_KEY,
@@ -15,11 +14,6 @@ from app.client.encrypt import (
     java_like_timestamp,
     get_x_signature_bounty
 )
-
-BASE_API_URL = os.getenv("BASE_API_URL")
-AX_DEVICE_ID = os.getenv("AX_DEVICE_ID")
-AX_FP = os.getenv("AX_FP")
-UA = os.getenv("UA")
 
 def get_payment_methods(
     api_key: str,
